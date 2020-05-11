81c18d51fce03560b7e3bf0814a142b14ebadd84
zhangwenxuan322 <798043150@qq.com>
2020-05-11 09:46:27 +0800

    add file2matrix function

--- k-近邻算法/kNN.py
+++ k-近邻算法/kNN.py
@@ -40,4 +40,19 @@ def classify0(inX, dataSet, labels, k):
                               )
     return sortedClassCount[0][0]
 
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOlines = fr.readlines
+    numberOfLines = len(arrayOlines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOlines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
 group,labels = createDataSet()
